279f7621a78c3e9c6a36090a7437519ff1a67a47
Bingran Hu <bingran.hu@yscope.com>
2025-02-01 04:13:07 -0500

    add array

--- src/ystdlib/hello.cpp
+++ src/ystdlib/hello.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <ystdlib/array/Array.hpp>
 #include <ystdlib/hello/hello.hpp>
 
 [[nodiscard]] auto main() -> int {
+    ystdlib::Array<std::string> a{4};
+    std::vector<std::string> const
+            std_vector{"yscope", "clp", "clp::Array", "default_initializable"};
+    std::copy(std_vector.cbegin(), std_vector.cend(), a.begin());
+    for (auto const& item : a) {
+        std::cout << item << '\n';
+    }
+
     std::cout << ystdlib::hello() << '\n';
     return 0;
 }
